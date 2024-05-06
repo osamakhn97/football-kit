5a2668f7c191bdd5f4cc503a63671dd26bc0a8e8
Osama <osamakhn97@gmail.com>
2024-05-06 10:55:59 +0500

    video updates

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.post("/generateVideo/async:id", createVideoAsync);
 app.post("/generateVideo/asynctemp:id", createVideoAsyncTemp);
 app.post("/generateVideo/chrome/async:id", createVideoChrome);
 app.post("/generateVideo1/chrome/async:id", createMediaChrome);
+app.get("/test",(req,res)=>res.send({msg:'connection successfull'}))
 app.get("/", getVideoUrl);
 app.post("/save", saveJson);
 
